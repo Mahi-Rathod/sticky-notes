f76bba36088467041518def28432032f401f88bb
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-10-12 21:46:47 +0530

    Implemented actual logic part of sticky notes bye adding new notes

--- app.js
+++ app.js
@@ -29,7 +29,61 @@ addBtn.innerText = "+";
 
 inputContainer.appendChild(addBtn);
 
+//Add Sticky notes we are using here localstorage for shake of simplycity one can use Api's for storing notes as per interest.
+
+//create a grid to show the sticky notes
+
+const notesContainer = document.createElement("div");
+notesContainer.classList.add("notes-container");
+
+main.appendChild(notesContainer);
+
+const colors = [
+    "#FFEB3B", // Yellow
+    "#FFC107", // Amber
+    "#FF5722", // Deep Orange
+    "#4CAF50", // Green
+    "#2196F3", // Blue
+    "#9C27B0", // Purple
+    "#E91E63", // Pink
+    "#00BCD4", // Cyan
+    "#8BC34A", // Light Green
+    "#F44336", // Red
+    "#FF9800", // Orange
+    "#3F51B5"  // Indigo
+  ];
+  
+
+const addStickyNotes = (notesContent) =>{
+    const newNote = document.createElement("div");
+    newNote.classList.add("sticky-note");
+    
+    notesContainer.appendChild(newNote);
+
+    //generate random number to pick the color from colors
+
+    const index = Math.floor(Math.random()*12);
+    newNote.style.backgroundColor = colors[index];
+
+    const notesTitle = document.createElement("p");
+    notesTitle.innerText = `${notesContent}`;
+    
+    newNote.appendChild(notesTitle);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "X";
+    newNote.appendChild(deleteBtn);
+
+    deleteBtn.addEventListener("click",()=>{
+        notesContainer.removeChild(newNote);
+    });
+}
+
+
 addBtn.addEventListener("click", ()=>{
     const notesContent = inputField.value;
-    console.log(notesContent);
+    if(notesContent.length >10)addStickyNotes(notesContent);
+    else alert("notes content should contain more than 10 alphabet.");
+
+    inputField.value = "";
 });
