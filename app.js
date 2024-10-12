2bec44f168f1f8cef1618e1ded1d8cdb80f6ede4
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-10-12 14:47:52 +0530

    created text area for input the notes title added button for adding new notes

--- app.js
+++ app.js
@@ -4,3 +4,32 @@ const heading = document.createElement("h1");
 heading.innerText = "Sticky Notes";
 
 container.appendChild(heading);
+
+//from branch feature2
+
+/* add input field and add button */
+
+const main = document.createElement("div");
+main.classList.add("main");
+
+container.appendChild(main);
+
+const inputContainer = document.createElement("div");
+inputContainer.classList.add("input-container");
+
+main.appendChild(inputContainer);
+
+const inputField = document.createElement("textarea");
+inputField.placeholder = "Type a new note...";
+
+inputContainer.appendChild(inputField);
+
+const addBtn = document.createElement("button");
+addBtn.innerText = "+";
+
+inputContainer.appendChild(addBtn);
+
+addBtn.addEventListener("click", ()=>{
+    const notesContent = inputField.value;
+    console.log(notesContent);
+});
